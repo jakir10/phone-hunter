0b48212fcd152f3428679f87ef4668f82f4fe98f
jakir10 <jakir.cse.bubt@gmail.com>
2022-03-01 16:16:19 +0600

    phone search and data show in ui from api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
-console.log('js is connected');
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // console.log(searchText);
+    searchField.value = '';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.data));
+
+}
+
+const displaySearchResult = data => {
+    const searchResult = document.getElementById('search-result');
+    data.forEach(data => {
+        console.log(data);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card h-100">
+                <img src="${data.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${data.brand}</h5>
+                    <p class="card-text">${data.phone_name}</p>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+
+
+
+
+
+
+    });
+
+}
